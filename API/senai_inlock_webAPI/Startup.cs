ce69d2a78f05f8ae1accc5b0ad8a593371fa9b3c
Kauê Camargo <camargokaue92@gmail.com>
2021-09-10 19:58:48 -0300

    Inlock Feito

---
+++ API/senai_inlock_webAPI/Startup.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webAPI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "inlock.webApi"
+
+                });   
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+
+            })
+                 .AddJwtBearer("JwtBearer", options => {
+                     options.TokenValidationParameters = new TokenValidationParameters
+                     {
+                         //validar quem esta emitindo o token
+                         ValidateIssuer = true,
+                         //validar quem esta recebendo o token
+                         ValidateAudience = true,
+                         //validar o tempo de expiracao
+                         ValidateLifetime = true,
+                         //definindo a chave de seguranca
+                         IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("sdajfhajkfsaahvuwafajcabvsafshfsakjfsa")),
+                         //tempo de expiracao do token
+                         ClockSkew = TimeSpan.FromMinutes(30),
+                         //define o nome de quem emite o token (issue)
+                         ValidIssuer = "inlock.webAPI",
+                         //define o nome de quem recebe o token (audience)
+                         ValidAudience = "inlock.webAPI"
+                     };
+                 });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "inlock.webAPI");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
